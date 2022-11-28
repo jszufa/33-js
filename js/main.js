c8c2c16be801861a944d1953527d4d20423d4357
jszufa <jerzy.szufa@gmail.com>
2022-11-28 15:26:10 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+paragraph = document.getElementById("opis");
+//console.log(paragraph);
+
+mainb = document.querySelector('button');
+//console.log(mainb);
+
+mainb.onclick = function (){
+    paragraph.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, quisquam sed? Quibusdam, nobis, eveniet odio molestias dolorem vel a laboriosam mollitia ex tempora deleniti, praesentium accusantium sequi illo quod quos!';
+}
